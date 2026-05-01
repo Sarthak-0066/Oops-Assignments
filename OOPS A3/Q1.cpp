815411ad4ff2fa96ffc206b0352ef2c54d774977
Sarthak-0066 <sgarg5_be24@thapar.edu>
2026-05-01 22:19:05 +0530

    Q1.cpp

---
+++ OOPS A3/Q1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+class Demo {
+    int x;
+public:
+    void set(int x) {
+        this->x = x; 
+    }
+    void display() {
+        cout << "Value: " << x << endl;
+    }
+};
+int main() {
+    Demo d;
+
+    d.set(10);
+    d.display();
+    Demo *ptr = &d;
+    ptr->set(20);
+    ptr->display();
+    return 0;
+}
